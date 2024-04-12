893d13984129f2d01c36b9c65b58df4f5157105c
J10100 <166766888+J10100@users.noreply.github.com>
2024-04-11 22:19:07 -0300

    Update Velocidade.java

--- App_Simulador_Carro/Modulo_Velocidade.java
+++ App_Simulador_Carro/Modulo_Velocidade.java
@@ -1,4 +1,34 @@
-package App_Simulador_Carro;
+package simuladorcorrida;
 
-public class Modulo_Velocidade {
+import java.util.Random;
+
+public class Veiculo {
+	private int velocidade;
+    private int distancia;
+    
+    public Veiculo() {
+        this.velocidade = 0;
+        this.distancia = 0;
+
+    }
+    
+    public int getDistancia() {
+    	return distancia;
+    }
+    
+    public void simularPasso() {
+        Random rand = new Random();
+        this.velocidade = rand.nextInt(3) + 1;
+        this.distancia += this.velocidade;
+    }
+    
+    public String representar() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < distancia; i++) {
+            sb.append(" ");
+        }
+        sb.append("V");
+        return sb.toString();
+    }
 }
+
