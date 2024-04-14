9d7112b55ca64c5673922d641a221715266cb34c
Júlia <166766888+J10100@users.noreply.github.com>
2024-04-14 20:47:28 -0300

    Add files via upload

---
+++ App_Simulador_Carro/SimuladorCorrida.java
@@ -0,0 +1,29 @@
+package simuladorcorrida;
+
+public class SimuladorCorrida {
+	private static final int DISTANCIA_MAXIMA = 50;
+	
+	public static void main (String[] args) {
+		Veiculo veiculo1 = new Veiculo();
+		Veiculo veiculo2 = new Veiculo();
+		
+		while(!veiculo1.alcancaDistanciaMaxima(DISTANCIA_MAXIMA) && !veiculo2.alcancaDistanciaMaxima(DISTANCIA_MAXIMA)) {
+			veiculo1.simularPasso();
+			veiculo2.simularPasso();
+			
+			System.out.println("Veículo 1: " + veiculo1.representar());
+			System.out.println("Veículo 2: " + veiculo2.representar());
+			System.out.println();
+		}
+		
+		if (veiculo1.alcancaDistanciaMaxima(DISTANCIA_MAXIMA) && veiculo2.alcancaDistanciaMaxima(DISTANCIA_MAXIMA)) {
+			System.out.println("Empate!");
+		}else if (veiculo1.alcancaDistanciaMaxima(DISTANCIA_MAXIMA)) {
+			System.out.println("Veículo 1 venceu!");
+		}else {
+			System.out.println("Veículo 2 venceu!");
+		}
+		
+	}
+
+}
